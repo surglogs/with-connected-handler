87f51b387c1208227feacd707365f8c8b7a7db2c
Erik Cupal <erik@cup.al>
2019-09-28 19:44:55 +0200

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ interface WithConnectedHandlerOptions {
   handler: (...connectedProps: any[]) => (...args: any[]) => any
   name: string
   actions?: { [actionName: string]: (...args: any[]) => any }
-  args?: { [connectedProp: string]: string | ((state, props) => any) }
+  args?: { [connectedProp: string]: string | ((state: object, props: object) => any) }
 }
 
 declare const withConnectedHandler: (options: WithConnectedHandlerOptions) => (component: any) => any
